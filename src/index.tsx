06cb571af67e6caa47ecbe1aca6180b304b55ce5
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-29 09:32:36 +0200

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,19 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 
-import { store } from './app/store';
+import './index.scss';
 import { App } from './App';
-import { UsersProvider } from './components/UsersContext';
+import { store } from './store/store';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 const Root = () => (
-  <Provider store={store}>
-    {/* Remove UsersProvider when you move users to Redux store */}
-    <UsersProvider>
-      <Router>
-        <App />
-      </Router>
-    </UsersProvider>
-  </Provider>
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
 );
 
 root.render(<Root />);
